c55494d741f1140f3038e3c3c9b6965ec28c9bdf
Ryan Caloras <rcaloras@gmail.com>
2016-01-16 23:12:29 -0500

    Imported old static content

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+var lessMiddleware = require('less-middleware');
+
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -19,6 +23,7 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(lessMiddleware(__dirname + '/public'));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
